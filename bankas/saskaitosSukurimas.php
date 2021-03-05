49c85708f1d8c70bac99056dcefcf52e438ce3b8
rugile <rugile.geceviciute@gmail.com>
2021-03-05 21:12:57 +0200

    create

--- bankas/saskaitosSukurimas.php
+++ bankas/saskaitosSukurimas.php
@@ -1,9 +1,18 @@
 <?php
+require __DIR__. '/bootstrap.php';
+ $randSaskaita = substr(str_shuffle(str_repeat('0123456789', mt_rand(1,18))), 1, 18);
+ if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $vardas = (string) $_POST['vardas'] ?? 'nera';
+    $pavarde = (string) $_POST['pavarde'] ?? 'nera';
+    $asmensKodas = (string) $_POST['asmensKodas'] ?? '00000000000';
+    $likutis = $_POST['likutis'] ?? 0;
+    create($vardas, $pavarde, $asmensKodas, $likutis); 
+    header('Location: '.URL);
+    die;
+
+}
 
 ?>
-
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,12 +25,12 @@
 </head>
 <body>
     <div class="topnav">
-        <a class="active" href="saskaituSarasas.php">Pagrindinis</a>
-        <a href="saskaitosSukurimas.php">Sukurti nauja saskaita</a>
-        <a href="pridetiLesas.php">Prideti lesas</a>
-        <a href="nuskaiciuotiLesas.php">Nuskaiciuoti lesas</a>
+        <a href="<?= URL ?>saskaituSarasas.php">Pagrindinis</a>
+        <a href="<?= URL ?>saskaitosSukurimas.php">Sukurti nauja saskaita</a>
+        <a href="<?= URL ?>pridetiLesas.php">Prideti lesas</a>
+        <a href="<?= URL ?>nuskaiciuotiLesas.php">Nuskaiciuoti lesas</a>
     </div>
-    <form action="" method="post">
+    <form action="<?= URL ?>saskaitosSukurimas.php" method="post">
         <h4>Įveskite vardą</h4>
         <input type="text" name="vardas"></input>
         <h4>Įveskite pavardę</h4>
@@ -29,7 +38,7 @@
         <h4>Jūsų sąskaitos numeris yra</h4>
         <input type="text" name="saskaitos numeris" value= <?= $randSaskaita ?> readonly></input>
         <h4>Įveskite asmens kodą</h4>
-        <input type="text" name="asmens kodas"></input>
+        <input type="text" name="asmensKodas"></input>
         <br></br><br>
         <button type="submit">SUKURTI</button>
         </br>
