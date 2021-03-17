b2277f3f85dcf54391c1b115231a7042d096679d
rugile <rugile.geceviciute@gmail.com>
2021-03-17 08:14:28 +0200

    Create edit.php

---
+++ bankas2/views/edit.php
@@ -0,0 +1,19 @@
+<?php require DIR.'views/top.php' ?>
+<?php require DIR.'views/menu.php' ?>
+
+    <form action="<?= URL ?>edit" method="post">
+        <h4>Įveskite vardą</h4>
+        <input type="text" name="vardas"></input>
+        <h4>Įveskite pavardę</h4>
+        <input type="text" name="pavarde"></input>
+        <h4>Jūsų sąskaitos numeris yra</h4>
+        <?php $randSaskaita = substr(str_shuffle(str_repeat('0123456789', mt_rand(1,18))), 1, 18); ?>
+        <input type="text" name="asmeninisId" value= <?= $randSaskaita ?> readonly></input>
+        <h4>Įveskite asmens kodą</h4>
+        <input type="text" name="asmensKodas"></input>
+        <br></br><br>
+        <button type="submit">REDAGUOTI</button>
+        </br>
+    </form>
+</body>
+</html>
